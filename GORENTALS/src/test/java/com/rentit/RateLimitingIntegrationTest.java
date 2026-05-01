63724385ad389e8b8da0203249ae653dff416dca
sunilkumar770 <sunilthoutam77@gmail.com>
2026-05-01 23:52:00 +0530

    fix(test): correct @AutoConfigureMockMvc annotation syntax

--- GORENTALS/src/test/java/com/rentit/RateLimitingIntegrationTest.java
+++ GORENTALS/src/test/java/com/rentit/RateLimitingIntegrationTest.java
@@ -2,7 +2,7 @@ package com.rentit;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoCo@AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,7 +12,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 @SpringBootTestt(classes = GoRentals.class)
-  nfigureMockMvc
+  @AutoConfigureMockMvc
 public class RateLimitingIntegrationTest {
 
     @Autowired
