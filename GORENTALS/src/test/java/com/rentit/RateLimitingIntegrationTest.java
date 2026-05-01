bc0375b5ec2f39f69931e78dc1d31624a193bea0
sunilkumar770 <sunilthoutam77@gmail.com>
2026-05-01 23:24:43 +0530

    fix(test): specify GoRentals class in @SpringBootTest annotation

--- GORENTALS/src/test/java/com/rentit/RateLimitingIntegrationTest.java
+++ GORENTALS/src/test/java/com/rentit/RateLimitingIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTes(classes = GoRentals.class)
 @AutoConfigureMockMvc
 public class RateLimitingIntegrationTest {
 
